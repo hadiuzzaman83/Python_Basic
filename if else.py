81f6d15a40a87728da5dbd660939ca9072a54351
Md Hadiuzzaman Sajib <78155287+hadiuzzaman83@users.noreply.github.com>
2021-06-05 23:16:36 +0600

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,15 @@
+num1=20
+num2=50
+
+if num1>num2:
+    print("num1 is greater")
+else:
+    print("num2 is greater")
+    
+num3=10
+num4=15
+
+if num3%2 ==0:
+    print("Number is even")
+else:
+    print("Number is Odd")
