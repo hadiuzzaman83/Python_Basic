e9fd6b61b25fe7fa29f72616f7eaddd2d76a114c
Md Hadiuzzaman Sajib <78155287+hadiuzzaman83@users.noreply.github.com>
2021-06-05 23:16:54 +0600

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,22 @@
+
+print("You can't take number over 100 !!!")
+val=int(input("Enter your number:"))
+
+if val>= 80:
+    print("A+")
+    
+elif val>=75:
+    print("A")
+elif val>=70:
+    print("A-")
+elif val>=65:
+    print("B+")  
+elif val>=60:
+    print("B")
+elif val>=55:
+    print("B-")
+elif val>=50:
+    print("C+")
+    
+else:
+    print("fail")
